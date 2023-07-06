e79ef08d937e198a423162e2f22e99a9220755b9
Sandro <sandromedeirosdacunha@gmail.com>
2023-07-05 22:09:55 -0300

    Entrega-Imc

--- src/app/form-imc/form-imc.component.ts
+++ src/app/form-imc/form-imc.component.ts
@@ -1,32 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-imc',
   templateUrl: './form-imc.component.html',
   styleUrls: ['./form-imc.component.scss']
 })
-export class FormImcComponent implements OnInit {
-  ngOnInit(): void {
-    
-  }
+export class FormImcComponent  {
+ 
 
-  public hover: string = "";
-  public nome: string = "";
-  pessoa ={
-    nome: ""
+  pessoa = {
+    nome:"",
+    peso: "",
+    altura: "",
+    sexo: "",
+    imc: 0,
+    avaliacao: ""
   }
+  pessoas: any = []
+  public hover: string = ""
 
   public isEmpty(): boolean{
-    if(this.pessoa.nome == ""){
-      return false;
+    if(this.pessoa.nome == "" 
+    || this.pessoa.peso == null 
+    || this.pessoa.peso == ""
+    || this.pessoa.altura == null 
+    || this.pessoa.altura == ""
+    || this.pessoa.sexo == ''){
+      return true;
     }
-    return true;
+    return false;
   }
 
   public classBtnHover(): string{
     if(!this.isEmpty()){
-      this.hover = "btn-hover";
+      return this.hover = "btn-hover";
     }
-    return this.hover;
+    return this.hover = "";
   }
+
+  public getForm(): void{
+    this.calcImc();
+    this.pessoas.push(this.pessoa)
+    this.cleanForm();
+    
+  }
+
+  public calcImc(): void{
+    this.pessoa.imc = Number( this.pessoa.peso) / Number(this.pessoa.altura) ** 2;
+    this.pessoa.imc = Number (this.pessoa.imc.toFixed(2))
+    if (this.pessoa.sexo == "m" && this.pessoa.imc < 19 || this.pessoa.sexo == "f" && this.pessoa.imc < 18) {
+      this.pessoa.avaliacao = 'Abaixo do peso';
+  } else if (this.pessoa.sexo == "m" && this.pessoa.imc < 25 || this.pessoa.sexo == "f" && this.pessoa.imc < 24) {
+      this.pessoa.avaliacao = 'Peso normal';
+  } else if (this.pessoa.sexo == "m" && this.pessoa.imc < 32 || this.pessoa.sexo == "f" && this.pessoa.imc < 29) {
+      this.pessoa.avaliacao = 'Sobrepeso';
+  } else {
+      this.pessoa.avaliacao = 'Obesidade';
+  }
+  }
+
+  public cleanForm(): void{
+    this.pessoa = {
+      nome:"",
+      peso: "",
+      altura: "",
+      sexo: "",
+      imc: 0,
+      avaliacao: ""
+    }
+  }
+
+ 
 }
--- src/app/table-imc/table-imc.component.ts
+++ src/app/table-imc/table-imc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-imc',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class TableImcComponent {
 
-}
+  @Input()
+  pessoa = {
+    nome:"Sandro Medeiros da Cunha",
+    peso: "",
+    altura: "",
+    sexo: "",
+    imc: 0,
+    avaliacao: "Avaliação"
+  }
+
+  public getImg(){
+    if(this.pessoa.avaliacao == "Peso normal"){
+      return "https://www.pngmart.com/files/15/Happy-Emoji-PNG.png"
+    }else{
+      return "https://freepngimg.com/thumb/sad_emoji/36900-6-sad-emoji-transparent-background-thumb.png"
+    }
+  }
+  }
+
+
