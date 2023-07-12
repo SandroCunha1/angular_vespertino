2b98e21787d478ff3c71e5e981b2df8184d962da
Sandro Cunha <sandromedeirosdacunha@gmail.com>
2023-07-12 17:27:48 -0300

    Fix Bugs

--- src/app/microondas/microondas.component.ts
+++ src/app/microondas/microondas.component.ts
@@ -3,29 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-microondas',
   templateUrl: './microondas.component.html',
-  styleUrls: ['./microondas.component.scss']
+  styleUrls: ['./microondas.component.scss'],
 })
 export class MicroondasComponent {
   public state = false;
   public min: number = 0;
   public sec: number = 0;
-  public total: number = this.min * 60 + this.sec;
   public secLegacy: number = 0;
+  public microInterval: any;
 
   transform(number: number): void {
     let min = this.min;
     let sec = this.sec;
+    this.secLegacy = sec;
 
     if (sec < 60) {
       sec = sec * 10 + number;
     }
-    if (sec < 100) {  
+    if (sec < 100) {
       min = min * 10 + Math.floor(sec / 60);
       sec %= 60;
     } else {
+      this.secLegacy = sec;
       min = min * 10 + Math.floor(sec / 100);
       sec %= 100;
-      if(sec >= 60){
+      if (sec >= 60) {
         min += Math.floor(sec / 60);
         sec %= 60;
       }
@@ -37,31 +39,30 @@ export class MicroondasComponent {
     }
   }
 
-  add30Seconds(){
+  add30Seconds() {
     this.sec = this.sec + 30;
-    if(this.sec >= 60){
+    if (this.sec >= 60) {
       this.min += Math.floor(this.sec / 60);
       this.sec %= 60;
     }
-    if(!this.state){
+    if (!this.state) {
       this.state = true;
       this.turnOn();
     }
   }
 
-  add01Minute(){
+  add01Minute() {
     this.min += 1;
-    if(!this.state){
+    if (!this.state) {
       this.state = true;
       this.turnOn();
     }
   }
 
-  
   turnOn() {
     this.state = true;
-    setInterval(() => {
-      if (this.state && this.sec > 0 || this.min > 0) {
+    if ((this.state && this.sec > 0) || this.min > 0) {
+      this.microInterval = setInterval(() => {
         this.sec -= 1;
         if (this.sec === -1 && this.min > 0) {
           this.min -= 1;
@@ -69,29 +70,19 @@ export class MicroondasComponent {
         }
         if (this.sec <= 0 && this.min <= 0) {
           this.state = false;
-          clearInterval;
+          clearInterval(this.microInterval);
         }
-      }
-    }, 1000);
-
+      }, 1000);
+    }
   }
 
   turnOff() {
     if (this.state) {
       this.state = false;
-      clearInterval;
+      clearInterval(this.microInterval);
     } else {
       this.sec = 0;
       this.min = 0;
     }
   }
-
 }
-
-
-
-
-
-
-
-
